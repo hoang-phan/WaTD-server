ade07d11491b4dc406959ae5241165dd53266ab0
Hoang Phan <hoangphanbk10@gmail.com>
2016-01-09 07:27:57 -0800

    [f] 514 - As a user, I want to update the owner's name of the image

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,7 +1,20 @@
 class ImagesController < ApplicationController
+  before_action :set_image, only: [:show, :update]
+
   def create
     image = Image.create(image_params)
     Recognizer.perform_async(image.id)
+    render json: { success: 'true' }
+  end
+
+  def show
+    send_data @image.data, type: 'image/jpg', disposition: 'inline'
+  end
+
+  def update
+    person = @image.person || Person.new
+    person.name = params[:name]
+    @image.update(person: person)
     render json: { success: 'true' }
   end
 
@@ -9,5 +22,9 @@ class ImagesController < ApplicationController
 
   def image_params
     params.require(:image).permit(:data)
+  end
+
+  def set_image
+    @image = Image.find(params[:id])
   end
 end
